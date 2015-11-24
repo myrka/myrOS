8c933615268a0c627ec1180bd8e9fd711e84989c
Sergiy Murygin <skyemurygin@gmail.com>
2015-11-24 17:58:15 +0200

    add clock

--- app/components/TaskPanel/TaskPanel.jsx
+++ app/components/TaskPanel/TaskPanel.jsx
@@ -1,10 +1,44 @@
 import './TaskPanel.less';
 
 export default class TaskPanel extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        };
+    }
+
+    componentWillMount() {
+        this._getTime();
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this._getTime();
+        }, 1000);
+    }
+
+    _getTime() {
+        const date = new Date();
+
+        this.setState({
+            hours: date.getHours(),
+            minutes: date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(),
+            seconds: date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+        });
+    }
+
+    _showDate() {
+        return <span className="clock">{this.state.hours}:{this.state.minutes}:{this.state.seconds}</span>;
+    }
+
     render() {
         return (
             <div className="task-panel">
-
+                {this._showDate()}
             </div>
         );
     }
